0e4e5dadade5387fa2fda0e2d37be07323b2383e
OndrejKacirek <bluepixeldev@gmail.com>
2025-12-08 16:45:36 +0100

    Initial version of nginx proxy showcase.

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+NASA_API_KEY = os.environ["NASA_API_KEY"]
+
+app = Flask(__name__)
+
+@app.route("/apod")
+def daily_image():
+    r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}&thumbs=true")
+    return r.json()
+
+if __name__ == "__main__":
+    app.run()
